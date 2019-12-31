e05f1a843421a3d070eb12f40119978a63476647
Frederik Jørgensen <201605647@post.au.dk>
2019-12-31 13:42:12 +0100

    New row and col system

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var numberOfWords = 7;
 var shuffleNumber = 50;
 var numberOfCorrects = 0;
 var bingo = false;
+var rows = 5;
+var columns = 2;
 
 var ord1 = ['Nytårsaften', 'Verden', 'Soldater', 'Kronprinsparret', 'Nytårshilsen', '2019', '2020', 'Kronprinsen', 'Nytårsønsker', 'Kronprinsessen', 'Unge', 'Klimaet', 'Prins Joachim', 'Gamle', 'Prinsesse Benedikte', 'Argentina', 'Arbejde', 'Europa', 'Glæde', 'Muligheder', 'Køge', 'Nyborg', 'Børnebørn', 'Sommer', 'Sydslesvig', 'Generationer', 'Politiet', 'Folk', 'Ansvar', 'Prinsesse Marie', 'Genforeningen', 'Grænsen', 'Tallinn', 'Flag', 'Mindretal', 'Sikkerhed'];
 var ord2 = ['Krig', 'Vejret', 'Flygtninge', 'Fred', 'Estland', 'Slesvig-Holsten', 'Særligt', 'Fællesskab', 'Klima', 'Frankrig', 'Dannebrog', 'Omsorg', 'Klimaforandringer', 'Jul', 'Kongeskibet', 'Sorg', 'Kronprins Frederik', 'Politi', 'Fødselsdag', 'Økonomiske'];
@@ -15,10 +17,10 @@ tableCreate();
 function tableCreate() {
     usedWords = [];
     var cellsToPopulate = [];
-    var nrOfDoubleCells = numberOfWords - 5;
+    var nrOfDoubleCells = numberOfWords - columns;
 
     for (i = 0; i < numberOfWords; i++) {
-        cellsToPopulate.push(randomNumber(3));
+        cellsToPopulate.push(randomNumber(rows));
     }
 
     var tbl = document.getElementById('table');
@@ -32,7 +34,6 @@ function tableCreate() {
 
     for (var k = 0; k < nrOfDoubleCells; k++) {
         var colAndRow = randomPlace();
-        console.log(colAndRow);
         tbl.rows[colAndRow[0]].cells[colAndRow[1]].innerHTML = randomWord(1);
     }
 
@@ -41,23 +42,29 @@ function tableCreate() {
 
 function checkTable() {
     var tbl = document.getElementById('table');
-    var rows = [];
+    var nrOfRows = [];
     var cols = [];
     for (var i = 1; i < tbl.rows.length; i++) {
         for (var j = 0; j < tbl.rows[i].cells.length; j++) {
             if (tbl.rows[i].cells[j].innerHTML !== "") {
-                rows.push(i);
+                nrOfRows.push(i);
                 cols.push(j);
             }
         }
     }
     var counts = [0];
-    rows.forEach(function(x) { counts[x] = (counts[x] || 0)+1; });
+    nrOfRows.forEach(function(x) { counts[x] = (counts[x] || 0)+1; });
     console.log(counts);
-    if (Math.max.apply(Math, counts) > 3) {
+    console.log(counts.length);
+    console.log("rows: " + rows);
+    if (Object.values(counts).length !== counts.length || counts.length < rows+1) { //rows +1 because of header
         console.log("making new");
         document.getElementById("newButton").click();
     }
+    // if (Math.min.apply(Math, counts) < 1) {
+    //     console.log("making new");
+    //     document.getElementById("newButton").click();
+    // }
 }
 
 function randomNumber(max) {
@@ -91,8 +98,8 @@ function randomPlace() {
     var tbl = document.getElementById("table");
     var arr = [];
     while (arr.length === 0) {
-        var row = randomNumber(3)-1;
-        var col = randomNumber(5)-1;
+        var row = randomNumber(rows); //first row is header
+        var col = randomNumber(columns)-1;
         if (tbl.rows[row].cells[col].innerHTML === "") {
             arr.push(row);
             arr.push(col);
